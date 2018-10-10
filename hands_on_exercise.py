8760758f59bcd85608e69b9b070dd5c4ea77da8c
Shivawasan <43978887+Shivawasan@users.noreply.github.com>
2018-10-10 10:05:45 +0530

    New_exercise

---
+++ hands_on_exercise.py
@@ -0,0 +1,43 @@
+"""Intro to Python - Part 1 - Hands-On Exercise."""
+
+
+import math
+import random
+
+
+# TODO: Write a print statement that displays both the type and value of `pi`
+pi = math.pi
+print(type(pi),pi)
+
+
+# TODO: Write a conditional to print out if `i` is less than or greater than 50
+i = random.randint(0, 100)
+if i < 50:
+    print("i is less than 50\n",i)
+if i>50:
+    print("i is greter than 50\n",i)
+
+
+# TODO: Write a conditional that prints the color of the picked fruit
+picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
+if picked_fruit == 'orange':
+    print(picked_fruit)
+elif picked_fruit =='strawberry':
+        print(picked_fruit)
+else:
+        print(picked_fruit)       
+# TODO: Write a function that multiplies two numbers and returns the result
+# Define the function here.
+
+def mul (a,b):
+    c=a*b
+    return c
+
+
+# TODO: Now call the function a few times to calculate the following answers
+
+print("12 x 96 =",mul(12,96))
+
+print("48 x 17 =",mul(48,17))
+
+print("196523 x 87323 =",mul(196523,87323))
--- test.py
+++ test.py
@@ -5,3 +5,7 @@ c = 12345.44
 d = True
 e = False
 print(a+b,"\n",c,"\n",d,"\n",e)
+def add(num1, num2):
+    result = num1 + num2
+    return result
+print(add(3,5))
